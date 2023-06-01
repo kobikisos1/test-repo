2e218256fb8db7b10717feeebc960a150cc3e649
kobikisos <kobi@sensi.ai>
2023-06-01 14:37:16 +0300

    added branch

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("started Main.start");
         System.out.println("add commit");
         System.out.println("add commit2");
+        System.out.println("add commit3");
     }
 }
