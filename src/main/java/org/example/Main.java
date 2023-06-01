4d4a9bed2ec6710a062b168cb418917ea186bd05
kobikisos <kobi@sensi.ai>
2023-06-01 17:03:00 +0300

    add commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args){
         System.out.println("started Main.start");
         System.out.println("add commit");
+        System.out.println("add commit2");
     }
 }
